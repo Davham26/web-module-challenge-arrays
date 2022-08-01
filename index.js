8d6f0037db80561234997e8d263eba547dfe705d
David Hammett <davham26@gmail.com>
2022-07-31 20:59:24 -0500

    task 3 completed

--- index.js
+++ index.js
@@ -83,11 +83,10 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-  /*your code here*/
- }
-
-
+function addFlavor(array, newFlav){
+  array.unshift(newFlav);
+  return array;
+}
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Houston, we have a problem! There are now 32 flavors in the originalFlavors array! Your task is to remove an item from the end of the array. 
